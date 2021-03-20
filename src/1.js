d380057aa9c7b177f61c6baf110fe1e3946f8b52
zkunsang <zkunsang@gmail.com>
2021-03-20 15:58:30 +0900

    temp

---
+++ src/1.js
@@ -0,0 +1,3 @@
+"use strict";
+var v1 = 123;
+var v2 = function () { return console.log("123"); };
---
+++ src/1.ts
@@ -0,0 +1,6 @@
+import { isEqual } from "lodash";
+
+const v1 = 123;
+const v2 = () => console.log("123");
+
+console.log(1 + 2);
